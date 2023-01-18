acc127e0cab945f1fb24737a64d60d7117ea6499
Naush <800007alvi@gmail.com>
2023-01-18 22:56:12 +0530

    fourth commit

--- src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -7,33 +7,33 @@ const FeaturedProducts = (type) => {
   const data = [
     {
       id: 1,
-      img: "",
-      img2: "",
+      img: "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg",
+      img2: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
       title: "Long sleeve graphic t-shirt",
       isNew:true,
       oldPrice: 19,
       price: 12,
     },
     {
-      id: 1,
-      img: "",
-      img2: "",
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw3bddkiuBvxsbvGHOFvtZzCyt3wR1fs66fWjrZ2Iu0axBuNoCNrjBXFH5hwJh55nUVh8&usqp=CAU",
+      img2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfvtuwzmb0CwIXYEd4ffnFZp7_PMScZaMmXmofo4Q82nrHgVUm12sK9mJB3ggDzj06zBU&usqp=CAU",
       title: "Shirt",
       isNew:true,
       oldPrice: 19,
       price: 12,
     },
     {
-      id: 1,
-      img: "",
-      img2: "",
+      id: 3,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThahkPvzhsaqfkCgENp9RfhN2WR7wOfm8gT_qF563wnobEVhsv04BtHSaW_kYCz6yYAug&usqp=CAU",
+      img2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHJLY2J7pFplMhwS-zEpVHVkltZdCzQ-c7LBHX8jUazCEalfIU8WPx-syGLacmq7xJKfY&usqp=CAU",
       title: "Hat",
       isNew:true,
       oldPrice: 19,
       price: 12,
     },
     {
-      id: 2,
+      id: 4,
       img: "",
       img2: "",
       title: "Coat",
@@ -57,11 +57,10 @@ const FeaturedProducts = (type) => {
       </p>
     </div>
     <div className="bottom">
-      {/* {error
-        ? "Something went wrong!"
-        : loading
-        ? "loading" */}
-        {data.map((item) => <Card item={item} key={item.id} />)}
+    
+        {data.map(item=>(
+          <Card item={item} key={item.id}/>
+        ))}
     </div>
   </div>
   );
